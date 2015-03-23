50486f40bea30269a0581a28a9f8d8357ad669ac
Pablo Castellano <pablo@anche.no>
2015-03-23 22:40:12 +0100

    Templates libreborme

--- libreborme/views.py
+++ libreborme/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render_to_response
 from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
-    template_name = "index.html"
+    template_name = "libreborme/index.html"
