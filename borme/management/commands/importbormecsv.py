904e020900eab084d0289857117834b18f754d33
Pablo Castellano <pablo@anche.no>
2015-03-25 17:05:18 +0100

    importbormecsv.py: arreglo algunos bugs y muestra el tiempo que ha tardado

--- borme/management/commands/importbormecsv.py
+++ borme/management/commands/importbormecsv.py
@@ -2,16 +2,17 @@
 
 from django.core.management.base import BaseCommand, CommandError
 from borme.models import Company, Borme, Acto, Person, Cargo
-from mongoengine.errors import ValidationError
+from mongoengine.errors import ValidationError, NotUniqueError
 
 import csv
 import os
 import re
+import time
 
 from borme_parser import ALL_KEYWORDS, CARGOS_KEYWORD
 
 RE_CARGOS_KEYWORD = '(%s)' % '|'.join(CARGOS_KEYWORD)
-RE_CARGOS_KEYWORD_NG = '(?:\.\s*%s|$)' % '|'.join(RE_CARGOS_KEYWORD) # FIXME:MAL pero funciona RE_...
+RE_CARGOS_KEYWORD_NG = '(?:\.\s*%s|$)' % '|'.join(RE_CARGOS_KEYWORD)  # FIXME:MAL pero funciona RE_...
 
 
 class Command(BaseCommand):
@@ -19,6 +20,7 @@ class Command(BaseCommand):
     help = 'Import CSV parsed BORME'
 
     def handle(self, *args, **options):
+        start_time = time.time()
 
         for filename in args:
             pdf_name = os.path.basename(filename).split('.pdf')[0]
@@ -39,6 +41,7 @@ class Command(BaseCommand):
             for row in csvr:
                 try:
                     #print "Importando acto con id", row['ID']
+                    #print row
 
                     #company = Company.objects.get_or_create(name=row['Nombre'])
                     #if not Company.objects.exists(name=row['Nombre']):
@@ -51,7 +54,12 @@ class Command(BaseCommand):
                     company.in_bormes = [pdf_name]
                     company.in_bormes.append(pdf_name)
                     company.in_bormes = list(set(company.in_bormes))
-                    company.save()
+                    try:
+                        company.save()
+                    except NotUniqueError, e:
+                        print 'ERROR creando empresa:', row['Nombre']
+                        print e
+                        continue
 
                     try:
                         acto = Acto.objects.get(borme=borme.filename, id_acto=row['ID'])
@@ -60,29 +68,34 @@ class Command(BaseCommand):
                         acto = Acto(company=company.slug, borme=borme.filename, id_acto=row['ID'])
 
                     for k in ALL_KEYWORDS:
-                        if row[k] != '':
+                        if row[k] not in (None, ''):
                             if k in ('Revocaciones', 'Reelecciones', 'Cancelaciones de oficio de nombramientos', 'Nombramientos'):
                                 print k, row[k]
                                 for match in re.finditer(RE_CARGOS_KEYWORD + ':\s+(.*?)' + RE_CARGOS_KEYWORD_NG, row[k]):
-                                    print match.group(1), match.group(2)
+                                    cargo, nombres = match.group(1), match.group(2)
+                                    print cargo, nombres
                                     l = []
-                                    for nombre in match.group(2).split(';'):
+                                    for nombre in nombres.split(';'):
                                         print '  ', nombre
                                         l.append(Cargo(titulo=match.group(1), nombre=nombre))
 
+                                        try:
+                                            p = Person.objects.get(name=nombre)
+                                        except Person.DoesNotExist:
+                                            print 'Creando persona:', nombre
+                                            p = Person(name=nombre)
+
+                                        p.in_companies.append(company.slug)
+                                        p.in_companies = list(set(p.in_companies))
+                                        p.in_bormes.append(pdf_name)
+                                        p.in_bormes = list(set(p.in_bormes))
+                                        try:
+                                            p.save()
+                                        except NotUniqueError, e:
+                                            print 'ERROR creando persona:', nombre
+                                            print e
                                     acto.__setattr__(k, l)
 
-                                    try:
-                                        p = Person.objects.get(name=nombre)
-                                    except Person.DoesNotExist:
-                                        print 'Creando persona:', nombre
-                                        p = Person(name=nombre)
-
-                                    p.in_companies.append(company.slug)
-                                    p.in_companies = list(set(p.in_companies))
-                                    p.in_bormes.append(pdf_name)
-                                    p.in_bormes = list(set(p.in_bormes))
-                                    p.save()
                             else:
                                 acto.__setattr__(k, row[k])
 
@@ -92,3 +105,7 @@ class Command(BaseCommand):
                     print e
 
             fp.close()
+
+        # Elapsed time
+        elapsed_time = time.time() - start_time
+        print '\nElapsed time: %.2f seconds' % elapsed_time
