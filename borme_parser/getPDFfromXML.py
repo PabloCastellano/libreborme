ba19858139e069591b8004d4c74395eaeb8e0aa2
Pablo Castellano <pablo@anche.no>
2015-03-25 16:18:09 +0100

    borme_parser: getPDFfromXML.py logging y resultados

--- borme_parser/getPDFfromXML.py
+++ borme_parser/getPDFfromXML.py
@@ -5,29 +5,52 @@ import os
 import datetime
 from lxml import etree
 import sys
+import logging
 
 start_time = time.time()
 
 # https
 URL = 'http://www.boe.es'
 
+LOGFILE = 'downloadpdf.log'
+LOGLEVEL = logging.INFO
+
+logging.basicConfig(filename=LOGFILE, level=LOGLEVEL, format='%(name)s: %(asctime)s %(message)s', datefmt='%Y-%m-%d %I:%M')
+logger = logging.getLogger(__name__)
+
+h1 = logging.StreamHandler(sys.stdout)
+h1.setLevel(LOGLEVEL)
+logger.addHandler(h1)
+
+results = {'error': 0, 'skip': 0, 'ok': 0, 'warning': 0}
+
+
+def get_borme_filename_xml(day):
+    return"BORME-S-" + day.strftime('%Y%m%d') + ".xml"
+
 
 def download_url(url):
-    filename = url.split('/')[-1]
-    filename = os.path.join('pdf', filename)
+    pdf_filename = url.split('/')[-1]
+    pdf_filename = os.path.join('pdf', pdf_filename)
 
-    if os.path.exists(filename):
-        print filename, 'already exists. Skipped.'
+    if os.path.exists(pdf_filename):
+        logger.debug('%s already exists. SKIP.' % pdf_filename)
+        results['skip'] += 1
         return False
 
-    print url,
-    r = requests.get(url, stream=True)
-    cl = r.headers.get('content-length')
-    print "%.2f KB" % (int(cl) / 1024.0)
+    logger.info(url)
+    try:
+        r = requests.get(url, stream=True)
+        cl = r.headers.get('content-length')
+        print "%.2f KB" % (int(cl) / 1024.0)
 
-    with open(filename, 'wb') as fd:
-        for chunk in r.iter_content(8192):
-            fd.write(chunk)
+        with open(pdf_filename, 'wb') as fd:
+            for chunk in r.iter_content(8192):
+                fd.write(chunk)
+    except requests.exceptions.ConnectionError:
+        logger.error('DOWNLOAD ERROR')
+        results['error'] += 1
+        return False
 
     return True
 
@@ -52,18 +75,24 @@ if __name__ == '__main__':
 
     total = 0
     while day <= end:
-        filename = "BORME-S-" + day.strftime('%Y%m%d') + ".xml"
+        filename = get_borme_filename_xml(day)
         filename = os.path.join('xml', filename)
-        print filename
+
+        if not os.path.isfile(filename):
+            logger.warning('%s NOT FOUND' % filename)
+            results['warning'] += 1
+            day += datetime.timedelta(days=1)
+            continue
 
         tree = etree.parse(filename)
         items = tree.xpath('/sumario/diario/seccion/emisor/item')
 
         for item in items:
             url = URL + item.getchildren()[1].text
-            converted = download_url(url)
-            if converted:
+            downloaded = download_url(url)
+            if downloaded:
                 total += 1
+                results['ok'] += 1
 
         day = tree.xpath('/sumario/meta/fechaSig')[0].text
         if day is None:
@@ -71,8 +100,19 @@ if __name__ == '__main__':
 
         day = datetime.datetime.strptime(day, '%d/%m/%Y')
         day = datetime.date.fromordinal(day.toordinal())
-        print 'sig:', str(day)
-        #time.sleep(0.05)
+        #print 'sig:', str(day)
+        time.sleep(0.05)
 
     elapsed_time = time.time() - start_time
-    print 'End. Elapsed time:', elapsed_time, "seconds. Converted %d files." % total
+    logger.info('End. Elapsed time: %s seconds. Downloaded %d files.' %(elapsed_time, total))
+
+    logger.info('\nElapsed time: %.2f seconds' % elapsed_time)
+    logger.info('Results:')
+    logger.info('  Downloaded: %d' % results['ok'])
+    logger.info('  Warning: %d' % results['warning'])
+    logger.info('  Skipped: %d' % results['skip'])
+
+    if results['error'] > 0:
+        logger.info('  Errors: %d (see %s for more information).' % (results['error'], LOGFILE))
+    else:
+        logger.info('  Errors: %d' % results['error'])
