59afe5d71d73322e72f75b8fd594f254eaafcf90
Pablo Castellano <pablo@anche.no>
2015-03-27 13:34:22 +0100

    mongonaut if DEBUG

--- libreborme/urls.py
+++ libreborme/urls.py
@@ -4,11 +4,14 @@ from django.contrib import admin
 admin.autodiscover()
 
 from views import IndexView
+from settings import DEBUG
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='home'),
     url(r'^b/', include('borme.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^mongonaut/', include('mongonaut.urls')),
 )
+
+if DEBUG:
+    urlpatterns += url(r'^mongonaut/', include('mongonaut.urls')),
