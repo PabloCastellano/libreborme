a7d82ddc953db20f323b830e2a95aa4a667beff6
Pablo Castellano <pablo@anche.no>
2015-03-29 13:37:50 +0200

    Usar django-bootstrap-static

--- libreborme/settings.py
+++ libreborme/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #mongoengine.django.mongo_auth,
+    'bootstrap',
     'borme',
     'mongogeneric',
     'mongodbforms',
