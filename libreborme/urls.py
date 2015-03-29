ac3a3434fdce0b6b245cdc0dc4edda3ccdcc8a5f
Pablo Castellano <pablo@anche.no>
2015-03-29 15:43:32 +0200

    añadido robots.txt

--- libreborme/urls.py
+++ libreborme/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import patterns, include, url
-
-from django.contrib import admin
-admin.autodiscover()
+from django.views.generic.base import TemplateView
 
 from views import IndexView
 from settings import DEBUG
@@ -10,7 +8,7 @@ urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='home'),
     url(r'^b/', include('borme.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'))
 )
 
 if DEBUG:
