a55a02d2344398d85b62e3b76a3b09bee7f6cebe
Pablo Castellano <pablo@anche.no>
2015-03-30 19:51:30 +0200

    diseño

--- borme/templatetags/utils.py
+++ borme/templatetags/utils.py
@@ -1,8 +1,22 @@
+# -*- coding: utf-8 -*-
+
 from django.template.defaulttags import register
 
+from borme_parser import DICT_KEYWORDS
+
+DICT_NAMES = {v: k for k, v in DICT_KEYWORDS.items()}
+DICT_NAMES['id_acto'] = 'ID acto'
 
 # http://stackoverflow.com/a/8000091
 @register.filter
 def get_item(object, attribute):
     return object.__getattribute__(attribute)
     #return dictionary.get(key)
+
+
+@register.filter
+def nombre(object):
+    try:
+        return DICT_NAMES[object]
+    except:
+        return object
