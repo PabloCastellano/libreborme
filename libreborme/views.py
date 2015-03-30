06eeb5ef134436916d1246a63e742ee0a74205a7
Pablo Castellano <pablo@anche.no>
2015-03-30 19:56:03 +0200

    Error404View

--- libreborme/views.py
+++ libreborme/views.py
@@ -5,3 +5,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "libreborme/index.html"
+
+
+class Error404View(TemplateView):
+    template_name = "libreborme/404.html"
