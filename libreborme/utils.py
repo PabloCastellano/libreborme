b85f0a1153665df044dcea655e0dc28500a6c260
Pablo Castellano <pablo@anche.no>
2015-07-05 16:48:09 +0200

    get_git_revision_short_hash devuelve Unknown

--- libreborme/utils.py
+++ libreborme/utils.py
@@ -1,7 +1,12 @@
 import subprocess
 
+
 def get_git_revision_short_hash():
-    version = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip()
-    if isinstance(version, bytes):
-        version = version.decode('unicode_escape')
+    try:
+        version = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip()
+        if isinstance(version, bytes):
+            version = version.decode('unicode_escape')
+    except subprocess.CalledProcessError:
+        # TODO: logging.warn()
+        version = 'Unknown'
     return version
