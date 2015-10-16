a1ea03cd7d8df67a8417072771a11b0c82e77bb9
Pablo Castellano <pablo@anche.no>
2015-10-16 02:03:18 +0200

    travis postgresql

--- runtests.sh
+++ runtests.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 python manage.py test borme.tests libreborme.tests --settings=libreborme.settings_test
-#python manage.py test libreborme.tests borme.tests --settings=libreborme.settings_test
-#python manage.py test app --testrunner=app.filename.NoDbTestRunner
-#python manage.py test libreborme.tests
 
 # coverage
 #coverage run --source='.' manage.py test myapp
