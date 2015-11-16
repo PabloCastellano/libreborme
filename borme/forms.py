42c573248212301dbc123aa1d7c22c5219d8a98b
Pablo Castellano <pablo@anche.no>
2015-11-16 19:56:51 +0100

    Formulario busqueda bootstrap

--- borme/forms.py
+++ borme/forms.py
@@ -2,14 +2,16 @@ from django import forms
 from django.apps import apps
 from django.utils.translation import ugettext_lazy as _
 
-from haystack.forms import SearchForm, model_choices
+from haystack.forms import SearchForm
 
 CHOICES = (('all', 'Todos'), ('company', 'Empresas'), ('person', 'Personas'),)
 
+
 class LBSearchForm(SearchForm):
+
     def __init__(self, *args, **kwargs):
         super(LBSearchForm, self).__init__(*args, **kwargs)
-        self.fields['type'] = forms.ChoiceField(choices=CHOICES, required=False, label=_('Search In'), widget=forms.RadioSelect)
+        self.fields['type'] = forms.ChoiceField(choices=CHOICES, required=False, label=_('Tipo'), widget=forms.RadioSelect)
 
     def get_models(self):
         """Return an alphabetical list of model classes in the index."""
