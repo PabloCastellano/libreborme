6bf8ac15fa115e71717d7d34abf77fcdb8c014de
Pablo Castellano <pablo@anche.no>
2015-12-02 19:45:38 +0100

    API search endpoints

--- borme/api/resources.py
+++ borme/api/resources.py
@@ -1,5 +1,10 @@
-from borme.models import Company, Person
+from django.conf.urls import url
+from django.core.paginator import Paginator, InvalidPage
+from django.http import Http404
+from haystack.query import SearchQuerySet
 from tastypie.resources import ModelResource
+from tastypie.utils import trailing_slash
+from borme.models import Company, Person
 from .serializers import LibreBormeJSONSerializer
 
 
@@ -10,6 +15,39 @@ class CompanyResource(ModelResource):
         allowed_methods = ['get']
         serializer = LibreBormeJSONSerializer(formats=['json'])
 
+    def prepend_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/search%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_search'), name="api_get_search_company"),
+        ]
+
+    def get_search(self, request, **kwargs):
+        self.method_check(request, allowed=['get'])
+        self.is_authenticated(request)
+        self.throttle_check(request)
+
+        # Do the query.
+        sqs = SearchQuerySet().models(Company).load_all().auto_query(request.GET.get('q', ''))
+        paginator = Paginator(sqs, 20)
+
+        try:
+            page = paginator.page(int(request.GET.get('page', 1)))
+        except InvalidPage:
+            raise Http404("Sorry, no results on that page.")
+
+        objects = []
+
+        for result in page.object_list:
+            bundle = self.build_bundle(obj=result.object, request=request)
+            bundle = self.full_dehydrate(bundle)
+            objects.append(bundle)
+
+        object_list = {
+            'objects': objects,
+        }
+
+        self.log_throttled_access(request)
+        return self.create_response(request, object_list)
+
 
 class PersonResource(ModelResource):
     class Meta:
@@ -17,3 +55,36 @@ class PersonResource(ModelResource):
         resource_name = 'persona'
         allowed_methods = ['get']
         serializer = LibreBormeJSONSerializer(formats=['json'])
+
+    def prepend_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/search%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_search'), name="api_get_search_person"),
+        ]
+
+    def get_search(self, request, **kwargs):
+        self.method_check(request, allowed=['get'])
+        self.is_authenticated(request)
+        self.throttle_check(request)
+
+        # Do the query.
+        sqs = SearchQuerySet().models(Person).load_all().auto_query(request.GET.get('q', ''))
+        paginator = Paginator(sqs, 20)
+
+        try:
+            page = paginator.page(int(request.GET.get('page', 1)))
+        except InvalidPage:
+            raise Http404("Sorry, no results on that page.")
+
+        objects = []
+
+        for result in page.object_list:
+            bundle = self.build_bundle(obj=result.object, request=request)
+            bundle = self.full_dehydrate(bundle)
+            objects.append(bundle)
+
+        object_list = {
+            'objects': objects,
+        }
+
+        self.log_throttled_access(request)
+        return self.create_response(request, object_list)
