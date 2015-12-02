7eb10ea0e235df0d2c131512fd42a618ca8478bd
Pablo Castellano <pablo@anche.no>
2015-12-02 20:57:34 +0100

    Hack para mostrar unos campos en la busqueda y todos en el endpoint normal

--- borme/api/resources.py
+++ borme/api/resources.py
@@ -6,21 +6,12 @@ from tastypie.utils import trailing_slash
 from borme.models import Company, Person
 from .serializers import LibreBormeJSONSerializer
 
-
+# FIXME: fullname
 class CompanyResource(ModelResource):
     class Meta:
         queryset = Company.objects.all()
         resource_name = 'empresa'
         allowed_methods = ['get']
-        serializer = LibreBormeJSONSerializer(formats=['json'])
-
-
-class SearchCompanyResource(ModelResource):
-    class Meta:
-        queryset = Company.objects.all()
-        resource_name = 'empresa'
-        allowed_methods = ['get']
-        fields = ['name', 'slug']  # TODO: fullname
         serializer = LibreBormeJSONSerializer(formats=['json'])
 
     def prepend_urls(self):
@@ -44,7 +35,7 @@ class SearchCompanyResource(ModelResource):
 
             for result in page.object_list:
                 bundle = self.build_bundle(obj=result.object, request=request)
-                bundle = self.full_dehydrate(bundle)
+                bundle = self.search_dehydrate(bundle)
                 objects.append(bundle)
         except:
             pass
@@ -56,21 +47,42 @@ class SearchCompanyResource(ModelResource):
         self.log_throttled_access(request)
         return self.create_response(request, object_list)
 
+    # Based on full_dehydrate
+    def search_dehydrate(self, bundle, for_list=False):
+        use_in = ['all', 'list' if for_list else 'detail']
+
+        for field_name, field_object in self.fields.items():
+            if field_name not in ('slug', 'name', 'resource_uri'):
+                continue
+
+            field_use_in = getattr(field_object, 'use_in', 'all')
+            if callable(field_use_in):
+                if not field_use_in(bundle):
+                    continue
+            else:
+                if field_use_in not in use_in:
+                    continue
+
+            if getattr(field_object, 'dehydrated_type', None) == 'related':
+                field_object.api_name = self._meta.api_name
+                field_object.resource_name = self._meta.resource_name
+
+            bundle.data[field_name] = field_object.dehydrate(bundle, for_list=for_list)
+
+            method = getattr(self, "dehydrate_%s" % field_name, None)
+
+            if method:
+                bundle.data[field_name] = method(bundle)
+
+        bundle = self.dehydrate(bundle)
+        return bundle
+
 
 class PersonResource(ModelResource):
     class Meta:
         queryset = Person.objects.all()
         resource_name = 'persona'
         allowed_methods = ['get']
-        serializer = LibreBormeJSONSerializer(formats=['json'])
-
-
-class SearchPersonResource(ModelResource):
-    class Meta:
-        queryset = Person.objects.all()
-        resource_name = 'persona'
-        allowed_methods = ['get']
-        fields = ['name', 'slug']
         serializer = LibreBormeJSONSerializer(formats=['json'])
 
     def dehydrate_name(self, bundle):
@@ -97,7 +109,7 @@ class SearchPersonResource(ModelResource):
 
             for result in page.object_list:
                 bundle = self.build_bundle(obj=result.object, request=request)
-                bundle = self.full_dehydrate(bundle)
+                bundle = self.search_dehydrate(bundle)
                 objects.append(bundle)
         except:
             pass
@@ -109,3 +121,33 @@ class SearchPersonResource(ModelResource):
 
         self.log_throttled_access(request)
         return self.create_response(request, object_list)
+
+    # Based on full_dehydrate
+    def search_dehydrate(self, bundle, for_list=False):
+        use_in = ['all', 'list' if for_list else 'detail']
+
+        for field_name, field_object in self.fields.items():
+            if field_name not in ('slug', 'name', 'resource_uri'):
+                continue
+
+            field_use_in = getattr(field_object, 'use_in', 'all')
+            if callable(field_use_in):
+                if not field_use_in(bundle):
+                    continue
+            else:
+                if field_use_in not in use_in:
+                    continue
+
+            if getattr(field_object, 'dehydrated_type', None) == 'related':
+                field_object.api_name = self._meta.api_name
+                field_object.resource_name = self._meta.resource_name
+
+            bundle.data[field_name] = field_object.dehydrate(bundle, for_list=for_list)
+
+            method = getattr(self, "dehydrate_%s" % field_name, None)
+
+            if method:
+                bundle.data[field_name] = method(bundle)
+
+        bundle = self.dehydrate(bundle)
+        return bundle
