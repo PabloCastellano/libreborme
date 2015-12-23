10b4e132f6776e5214fcfce9e1afc9dd2c57469b
Pablo Castellano <pablo@anche.no>
2015-12-23 20:16:19 +0100

    politica de cookies

--- libreborme/urls.py
+++ libreborme/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic.base import TemplateView
 
-from .views import IndexView
+from .views import IndexView, CookiesView
 from .settings import DEBUG
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='home'),
     url(r'^borme/', include('borme.urls')),
 
-    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'))
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    url(r'^politica-de-cookies$', CookiesView.as_view(), name='cookies')
 )
 
 if DEBUG:
--- libreborme/views.py
+++ libreborme/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "libreborme/index.html"
+
+
+class CookiesView(TemplateView):
+    template_name = "libreborme/cookies.html"
