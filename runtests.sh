b316a5bd6d08e58c71a37fcdbbf4287aba498394
Pablo Castellano <pablo@anche.no>
2016-03-09 18:46:26 +0100

    añadido tests/__init__.py

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./manage.py test borme.tests --settings=libreborme.settings_test
+./manage.py test borme --settings=libreborme.settings_test
 # python manage.py test borme.tests.test_person_model.TestPersonModel --settings=libreborme.settings_test
 # python manage.py test borme.tests.test_company_model.TestCompanyModel --settings=libreborme.settings_test
 
