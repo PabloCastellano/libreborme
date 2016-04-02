9a0381c52afff6e82bbc2f0f570a4e398390b964
Pablo Castellano <pablo@anche.no>
2016-04-02 18:32:36 +0200

    Static pages: about, contact, developers, services, support, supporters
    Conflicts:
    	libreborme/urls.py

--- libreborme/urls.py
+++ libreborme/urls.py
@@ -1,16 +1,22 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic.base import TemplateView
 
-from .views import IndexView, CookiesView
 from .settings import DEBUG
 
 urlpatterns = patterns('',
-    url(r'^$', IndexView.as_view(), name='home'),
+    url(r'^$', TemplateView.as_view(template_name="libreborme/index.html"), name='home'),
     url(r'^borme/', include('borme.urls')),
 
     url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     url(r'^humans\.txt$', TemplateView.as_view(template_name='humans.txt', content_type='text/plain')),
-    url(r'^politica-de-cookies$', CookiesView.as_view(), name='cookies')
+
+    url(r'^about/$', TemplateView.as_view(template_name="libreborme/about.html"), name='about'),
+    url(r'^contact/$', TemplateView.as_view(template_name="libreborme/contact.html"), name='contact'),
+    url(r'^cookies/$', TemplateView.as_view(template_name="libreborme/cookies.html"), name='cookies'),
+    url(r'^developers/$', TemplateView.as_view(template_name="libreborme/developers.html"), name='developers'),
+    url(r'^services/$', TemplateView.as_view(template_name="libreborme/services.html"), name='services'),
+    url(r'^support/$', TemplateView.as_view(template_name="libreborme/support.html"), name='support'),
+    url(r'^supporters/$', TemplateView.as_view(template_name="libreborme/supporters.html"), name='supporters'),
 )
 
 if DEBUG:
--- libreborme/views.py
+++ libreborme/views.py
@@ -1,9 +1,1 @@
-from django.views.generic import TemplateView
 
-
-class IndexView(TemplateView):
-    template_name = "libreborme/index.html"
-
-
-class CookiesView(TemplateView):
-    template_name = "libreborme/cookies.html"
