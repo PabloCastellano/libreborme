26e62c43751a4f963b894b319f313ad5b2a2089d
Pablo Castellnano <pablo@anche.no>
2017-02-25 17:12:12 +0100

    Vistas dashboard

--- views.py
+++ views.py
@@ -7,8 +7,28 @@ from django.views.generic.list import ListView
 from django.views.generic import TemplateView
 
 from .models import AlertaActo, AlertaCompany, AlertaPerson
+from . import forms
 
 
+@method_decorator(login_required, name='dispatch')
+class DashboardView(TemplateView):
+    template_name = 'alertas/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        n_alertas = AlertaActo.objects.filter(user=self.request.user).count()
+        n_alertas += AlertaCompany.objects.filter(user=self.request.user).count()
+        n_alertas += AlertaPerson.objects.filter(user=self.request.user).count()
+        context['n_alertas'] = n_alertas
+        return context
+
+
+@method_decorator(login_required, name='dispatch')
+class BillingView(TemplateView):
+    template_name = 'alertas/billing.html'
+
+
+@method_decorator(login_required, name='dispatch')
 class AlertaDetailView(DetailView):
     model = AlertaActo
     context_object_name = 'alerta'
@@ -35,7 +55,22 @@ class AlertaListView(TemplateView):
         context['breadcrumb'] = 'TODO BREADCRUMB'
         return context
 
+@login_required
+def alerta_create(request):
+    context = {}
+    context['form_c'] = forms.AlertaCompanyForm()
+    context['form_p'] = forms.AlertaPersonForm()
+    context['form_a'] = forms.AlertaActoForm()
 
-class AlertaCompanyCreateView(CreateView):
+    return render(request, 'alertas/alerta_new.html', context)
+
+
+@method_decorator(login_required, name='dispatch')
+class AlertaActoCreateView(CreateView):
     model = AlertaCompany
-    fields = ("user", "company", "send_html")
+    fields = ("company", "send_html")
+
+# No vale CreateView porque la lista para elegir empresa/persona sería inmensa 
+
+# #        - En liquidación
+#        - Concurso de acreedores
