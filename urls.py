0b5d846d51486f2ddb36713e7161a92bc0ae5c14
Pablo Castellnano <pablo@anche.no>
2017-02-25 14:23:19 +0100

    dashboard init

--- urls.py
+++ urls.py
@@ -3,8 +3,9 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^alertas/$', views.AlertaListView.as_view(), name='alertas-index'),
-    url(r'^alertas/new/$', views.AlertaCompanyCreateView.as_view(), name='alertas-index'),
+    url(r'^alertas/$', views.DashboardView.as_view(), name='alertas-index'),
+    url(r'^alertas2/$', views.AlertaListView.as_view(), name='alertas-index2'),
+    url(r'^alertas/new/$', views.alerta_create, name='alertas-index'),
     url(r'^alertas/(?P<id>\d+)/$', views.AlertaDetailView.as_view(), name='alertas-detail'),
 #    url(r'^$', views.alertas_index, name='alertas-index'),
 #    url(r'^(?P<id>\d+)/$', views.alertas_view, name='alertas-detail'),
