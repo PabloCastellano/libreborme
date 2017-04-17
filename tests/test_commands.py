e4a2f5a10fc0e9388e2bebc80c196d636a9943ac
Pablo Castellano <pablo@anche.no>
2017-04-17 21:09:07 +0200

    Added tests

---
+++ tests/test_commands.py
@@ -0,0 +1,74 @@
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.management import call_command
+
+from django.test import TestCase
+
+from alertas.models import Profile
+from alertas.utils import create_alertas_user
+
+from django.utils import timezone
+
+
+class TestCommandCreateAlertasUser(TestCase):
+
+    def test_user_is_created(self):
+        users = User.objects.all()
+        self.assertEqual(len(users), 0)
+        profiles = Profile.objects.all()
+        self.assertEqual(len(profiles), 0)
+
+        call_command('create_alertas_user', username="fred", email="fred@localhost",
+                     first_name='Fred', last_name="Foo", password="secret")
+
+        users = User.objects.all()
+        self.assertEqual(len(users), 1)
+        profiles = User.objects.all()
+        self.assertEqual(len(profiles), 1)
+
+        user = User.objects.first()
+        self.assertEqual(user.username, "fred")
+        self.assertEqual(user.email, "fred@localhost")
+        self.assertEqual(user.first_name, "Fred")
+        self.assertEqual(user.last_name, "Foo")
+
+        profile = Profile.objects.first()
+        self.assertEqual(profile.account_type, "test")
+        self.assertEqual(profile.notification_method, "email")
+        self.assertEqual(profile.notification_email, "fred@localhost")
+
+
+class TestCommandExpireTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestCommandExpireTest, cls).setUpClass()
+
+        create_alertas_user("fred", "fred@localhost", "secret", "Fred", "Foo", "test")
+        john = create_alertas_user("john", "johnd@localhost", "secret", "John", "Foo", "test")
+        john.date_joined = timezone.now() - timezone.timedelta(days=30) # TODO: EXPIRE_AFTER_DAYS
+        john.save()
+
+    def test_user_expired(self):
+        users = User.objects.filter(is_active=True)
+        self.assertEqual(len(users), 2)
+
+        call_command('expire_test_users')
+
+        users = User.objects.filter(is_active=True)
+        self.assertEqual(len(users), 1)
+
+        john = User.objects.get(username="john")
+        self.assertEqual(john.is_active, False)
+
+
+"""
+class TestCommandNotifications(TestCase):
+    
+    def test_send_notifications(self):
+        self.assertEqual(1, 1)
+
+        call_command('send_notifications')
+
+        # test emails sent
+"""
---
+++ tests/test_http.py
@@ -0,0 +1,122 @@
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.management import call_command
+from django.core.urlresolvers import reverse
+
+from django.test.client import Client
+from django.test import TestCase
+
+from alertas.utils import create_alertas_user
+
+
+class TestAlertasHttp(TestCase):
+
+    fixtures = ['alertasconfig.json']
+
+    def setUp(self):
+        super(TestAlertasHttp, self).setUp()
+        self.user = create_alertas_user("fred", "fred@localhost", "secret", "Fred", "Foo", "test")
+        self.client = Client()
+        self.client.login(username='fred', password='secret')
+
+    def test_index(self):
+        url = reverse('dashboard-index')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+
+class TestAlertasLoginRequired(TestCase):
+
+    def setUp(self):
+        self.login_url = reverse('login')
+        # self.client = Client()
+
+    def test_alertas(self):
+        url = reverse('dashboard-index')
+        response = self.client.get(url)
+        self.assertTrue(response.url.startswith(self.login_url))
+
+    def test_alertas_id(self):
+        url = reverse('alertas-detail', args=['1'])
+        response = self.client.get(url)
+        self.assertTrue(response.url.startswith(self.login_url))
+
+    def test_alertas_ayuda(self):
+        url = reverse('alertas-ayuda')
+        response = self.client.get(url)
+        self.assertTrue(response.url.startswith(self.login_url))
+
+    def test_alertas_billing(self):
+        url = reverse('alertas-billing')
+        response = self.client.get(url)
+        self.assertTrue(response.url.startswith(self.login_url))
+
+    def test_alertas_invoice_view(self):
+        url = reverse('alertas-invoice-view', args=['1'])
+        response = self.client.get(url)
+        self.assertTrue(response.url.startswith(self.login_url))
+
+    def test_alertas_dashboard_history(self):
+        url = reverse('dashboard-history')
+        response = self.client.get(url)
+        self.assertTrue(response.url.startswith(self.login_url))
+
+    def test_alertas_history_download(self):
+        url = reverse('alerta-history-download', args=['1'])
+        response = self.client.get(url)
+        self.assertTrue(response.url.startswith(self.login_url))
+
+    def test_alertas_new_acto(self):
+        url = reverse('alertas-new-acto')
+        response = self.client.get(url)
+        self.assertTrue(response.url.startswith(self.login_url))
+
+    def test_alertas_new_company(self):
+        url = reverse('alertas-new-company')
+        response = self.client.get(url)
+        self.assertTrue(response.url.startswith(self.login_url))
+
+    def test_alertas_new_person(self):
+        url = reverse('alertas-new-person')
+        response = self.client.get(url)
+        self.assertTrue(response.url.startswith(self.login_url))
+
+    def test_alertas_remove_acto(self):
+        url = reverse('alerta-remove-acto', args=['1'])
+        response = self.client.get(url)
+        self.assertTrue(response.url.startswith(self.login_url))
+
+    def test_alertas_remove_company(self):
+        url = reverse('alerta-remove-company', args=['1'])
+        response = self.client.get(url)
+        self.assertTrue(response.url.startswith(self.login_url))
+
+    def test_alertas_remove_person(self):
+        url = reverse('alerta-remove-person', args=['1'])
+        response = self.client.get(url)
+        self.assertTrue(response.url.startswith(self.login_url))
+
+    def test_alertas_settings(self):
+        url = reverse('alertas-settings')
+        response = self.client.get(url)
+        self.assertTrue(response.url.startswith(self.login_url))
+
+    def test_alertas_settings_notifications(self):
+        url = reverse('alertas-settings-notifications')
+        response = self.client.get(url)
+        self.assertTrue(response.url.startswith(self.login_url))
+
+    def test_alertas_settings_personal(self):
+        url = reverse('alertas-settings-personal')
+        response = self.client.get(url)
+        self.assertTrue(response.url.startswith(self.login_url))
+
+    def test_alertas_ajax_suggest_company(self):
+        url = reverse('suggest_company')
+        response = self.client.get(url)
+        self.assertTrue(response.url.startswith(self.login_url))
+
+    def test_alertas_ajax_suggest_person(self):
+        url = reverse('suggest_person')
+        response = self.client.get(url)
+        self.assertTrue(response.url.startswith(self.login_url))
