c32a20dd85ae14430dfff57c90ece48d26d4cc1c
Pablo Castellano <pablo@anche.no>
2018-08-30 21:06:21 +0200

    fix: replace libreborme.net by localhost in settings_dev

--- libreborme/settings_dev.py
+++ libreborme/settings_dev.py
@@ -21,7 +21,7 @@ SECRET_KEY = '41+h()yq5-!*=)sh+_%4wal8=+*e)dlrau*81odpu7n&9^7d5h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'libreborme.net', '.ngrok.io']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.ngrok.io']
 
 
 # Application definition
