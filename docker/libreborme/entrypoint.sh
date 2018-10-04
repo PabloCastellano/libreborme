4fe539c0ff5d64886c1a1884236deaaf752bc9cd
Pablo Castellano <pablo@anche.no>
2018-10-04 15:12:24 +0200

    docker entrypoint use env variables

--- docker/libreborme/entrypoint.sh
+++ docker/libreborme/entrypoint.sh
@@ -4,8 +4,20 @@ set -e
 
 crond
 
+if [ -n "$ELASTICSEARCH_URI" ]; then
+	ES_HOST=$(echo "$ELASTICSEARCH_URI" | sed 's/.*@\(.*\).*/\1/')
+else
+	ES_HOST=elasticsearch:9200
+fi
+
+if [ -n "$DB_HOST" -a -n "$DB_PORT" ]; then
+	PG_HOST="$DB_HOST:$DB_PORT"
+else
+	PG_HOST=postgres:5432
+fi
+
 cd /site
-./scripts/wait-for-it.sh elasticsearch:9200 --timeout=30
-./scripts/wait-for-it.sh postgres:5432 --timeout=30
+./scripts/wait-for-it.sh $ES_HOST --timeout=30
+./scripts/wait-for-it.sh $PG_HOST --timeout=30
 ./manage.py migrate
 uwsgi --ini=/site/uwsgi.ini
