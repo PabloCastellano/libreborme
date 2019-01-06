8e858dae184170225627eaee584fc6a921680b75
Pablo Castellano <pablo@anche.no>
2019-01-06 23:37:04 +0100

    chore: only print "Borme ready" when DEBUG = True

--- borme/apps.py
+++ borme/apps.py
@@ -2,6 +2,10 @@ from django.apps import AppConfig
 
 from borme.utils.sanity import check_permissions
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class BormeConfig(AppConfig):
     name = 'borme'
@@ -9,4 +13,4 @@ class BormeConfig(AppConfig):
     def ready(self):
         # Note that it runs twice, check https://stackoverflow.com/questions/43577426/appconfig-ready-is-running-twice-on-django-setup-using-heroku/46206432
         check_permissions()
-        print('Borme is ready')
+        logger.debug('Borme is ready')
