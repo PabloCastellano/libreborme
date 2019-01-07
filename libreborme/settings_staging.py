06a6c82c297fbec31931dad885525395880c459a
Pablo Castellano <pablo@anche.no>
2019-01-07 20:33:53 +0100

    fix: settings_override in staging

--- libreborme/settings_staging.py
+++ libreborme/settings_staging.py
@@ -6,11 +6,6 @@ from bormeparser.config import CONFIG
 
 from .settings import *
 
-try:
-    from .settings_override import *
-except ImportError:
-    pass
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '41+h()yq5-!*=)sh+_%4wal8=+*e)dlrau*81odpu7n&9^7d5h'
 
@@ -205,3 +200,8 @@ LOGGING = {
         },
     }
 }
+
+try:
+    from .settings_override import *
+except ImportError:
+    pass
