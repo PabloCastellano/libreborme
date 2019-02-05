22d62237b26d414070afc8688d509b49f6ef841a
Pablo Castellano <pablo@anche.no>
2019-02-05 19:52:42 +0100

    setuptools: remove install_requires

--- setup.py
+++ setup.py
@@ -4,24 +4,7 @@ from setuptools import setup, find_packages
 import libreborme
 import sys
 
-version = '20180531.dev0'
-
-
-def get_install_requires():
-    """
-    parse requirements.txt, ignore links, exclude comments
-    """
-    requirements = []
-
-    for line in open('requirements/base.txt').readlines():
-        # skip to next iteration if comment or empty line
-        if line.startswith('#') or line == '' or line.startswith('http') or line.startswith('git'):
-            continue
-
-        # add line to requirements
-        requirements.append(line)
-
-    return requirements
+version = '20190205.dev0'
 
 
 if sys.version_info[0] == 3:
@@ -52,7 +35,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP',
     ],
-    install_requires=get_install_requires(),
     scripts=['libreborme/bin/libreborme'],
     test_suite='runtests.runtests',
 )
