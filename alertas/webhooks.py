645089055fc42a7e6280d93e5b211da6c3ed2569
Pablo Castellano <pablo@anche.no>
2019-03-06 00:26:52 -0300

    webhooks

--- alertas/webhooks.py
+++ alertas/webhooks.py
@@ -5,6 +5,9 @@ Docs:
 https://github.com/dj-stripe/dj-stripe/issues/791
 https://github.com/dj-stripe/dj-stripe/issues/782
 https://github.com/dj-stripe/dj-stripe/issues/419
+
+https://stackoverflow.com/questions/19161630/how-to-prevent-manage-stripe-webhook-sending-invoice-for-0-on-trial-signup
+https://stackoverflow.com/questions/19467287/stripe-how-to-handle-subscription-with-a-free-plan-and-no-credit-card-required
 """
 from django.core.mail import mail_admins
 
@@ -46,10 +49,23 @@ def customer_subscription_updated(event, **kwargs):
     message = """\
 Cambio en la suscripción
 
-El {} se ha producido un cambio en el suscriptor {} ({})
+El {} se ha producido un cambio en el suscriptor {} ({}).
+
+Cambios:
+{}
 """.format(
-        now.strftime("%c"), full_name, user.email)
+        now.strftime("%c"), full_name, user.email, event.data['previous_attributes'])
     mail_admins(subject, message)
+    # TODO: Set UserSubscription.active = False
+
+
+@webhooks.handler("customer.subscription.deleted")
+def customer_subscription_deleted(event, **kwargs):
+    """
+    For example when subscription is cancelled using the Stripe dashboard
+    TODO: Set UserSubscription.active = False
+    """
+    pass
 
 
 # https://stripe.com/docs/recipes/sending-emails-for-failed-payments
@@ -63,7 +79,6 @@ def invoice_payment_failed(event, **kwargs):
     user = event.customer.subscriber
     full_name = user.get_full_name()
     subject = 'Libreborme webhook triggered: ' + event.type
-    subject = 'Payment failed ({}) (webhook)'.format(full_name)
     message = """\
 Pago fallido
 
@@ -105,6 +120,45 @@ Nuevo customer
 El {} se ha creado un nuevo customer {}.
 """.format(
         now.strftime("%c"), customer.email)
+    mail_admins(subject, message)
+
+
+@webhooks.handler("customer.subscription.trial_will_end")
+def customer_subscription_trial_will_end(event, **kwargs):
+    """
+    Sent three days before the trial period is up
+    Notify user
+    """
+    user = event.customer.subscriber
+
+    # TODO: Usar MailTemplate's
+    # TODO: redactar
+    subject = "Tu periodo de prueba va a finalizar pronto"
+    message = """Blablabla... upcoming invoice día y cantidad
+"""
+    # user.email_user(
+    #     subject=subject,
+    #     message=message
+    # )
+
+
+@webhooks.handler("charge.succeeded")
+def charge_succeeded(event, **kwargs):
+    """
+    Sent by Stripe one hour after invoice.created
+    Notify to admins
+    """
+    print("Webhook " + event.type)
+    now = datetime.datetime.now()
+    user = event.customer.subscriber
+    full_name = user.get_full_name()
+    subject = 'Libreborme webhook triggered: ' + event.type
+    message = """\
+Pago realizado
+
+El {} se ha realizado un pago con éxito del customer {} ({}).
+""".format(
+        now.strftime("%c"), full_name, user.email)
     mail_admins(subject, message)
 
 
